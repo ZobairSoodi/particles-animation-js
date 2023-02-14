5833adad4efbc9d5bc7a7bb5e3fe81eecc243995
ZobairSoodi <soodi.zobair.solicode@gmail.com>
2023-02-14 09:57:05 +0000

    d

--- p5/stars + meteor shower/sketch.js
+++ p5/stars + meteor shower/sketch.js
@@ -5,13 +5,12 @@ let shootingStars = [];
 let timer = 1000;
 let blackHole = undefined;
 let idCount = 0;
-let numStars = 150;
+let numStars = 50;
 let starCooldown = 1000;
 
 function setup() {
   let myCanvas = createCanvas(windowWidth, windowHeight);
   myCanvas.parent("canvas-parent");
-  background('#222629');
 
   for (let i = 0; i < numStars; i++) {
     stars.push(new Star());
@@ -19,22 +18,22 @@ function setup() {
 }
 
 function draw() {
-  background('black');
-  fill('white');
+  background('#efefef');
+  fill('black');
 
   if (millis() > starCooldown) {
     shootingStars.push(new ShootingStar());
     starCooldown = millis() + random(3000, 10000);
   }
 
-  if(mouseIsPressed && floor(millis()) % 5 === 0 ){
+  if (mouseIsPressed && floor(millis()) % 5 === 0) {
     shootingStars.push(new ShootingStar());
   }
 
   shootingStars.forEach((ss) => {
     ss.display();
     ss.move();
-  })
+  });
 
   stars.forEach((s) => {
     s.display();
@@ -54,7 +53,7 @@ class Star {
     this.r = random(1, 3);
     this.opacity = 100;
     this.glowCounter = 0;
-    this.makeGlow = random(0, 1000);
+    this.makeGlow = random(0, 500);
     this.isGlowing = false;
     this.isFading = false;
     this.glowOpacity = 0;
@@ -66,8 +65,9 @@ class Star {
       this.isGlowing = true;
       this.makeGlow = random(0, 1000);
     }
+
     noStroke();
-    fill(255, 255, 255, this.opacity);
+    fill(0, 0, 0, this.opacity);
     for (let i = 1; i < 5; i++) {
       ellipse(this.pos.x, this.pos.y, this.r * i / 2);
     }
@@ -75,13 +75,13 @@ class Star {
     if (this.isGlowing) {
       this.glowCounter = 0;
       this.glowOpacity += 1;
-      fill(255, 255, 255, this.glowOpacity);
+      fill(0, 0, 0, this.glowOpacity);
       ellipse(this.x, this.y, this.r * 1.5);
-      star(this.pos.x, this.pos.y, this.r * 4, this.r / 2, 4);
-      fill(255, 255, 255, this.glowOpacity / 2);
-      star(this.pos.x, this.pos.y, this.r * 4, this.r, 4);
-      fill(255, 255, 255, this.glowOpacity / 4);
-      star(this.pos.x, this.pos.y, this.r * 5, this.r * 2, 4);
+      star(this.pos.x, this.pos.y, this.r * 2, this.r / 2, 4);
+      fill(0, 0, 0, this.glowOpacity / 2);
+      star(this.pos.x, this.pos.y, this.r * 3, this.r, 4);
+      fill(0, 0, 0, this.glowOpacity / 4);
+      star(this.pos.x, this.pos.y, this.r * 4, this.r * 2, 4);
       if (this.glowOpacity >= 150) {
         this.isGlowing = false;
         this.isFading = true;
@@ -89,9 +89,11 @@ class Star {
     }
     else if (this.isFading) {
       this.glowOpacity -= 2;
-      fill(255, 255, 255, this.glowOpacity);
+      fill(0, 0, 0, this.glowOpacity);
       ellipse(this.pos.x, this.pos.y, this.r * 1.5)
       star(this.pos.x, this.pos.y, this.r * 4, this.r, 4);
+      fill(0, 0, 0, this.glowOpacity / 2);
+      star(this.pos.x, this.pos.y, this.r * 2, this.r, 4);
       if (this.glowOpacity <= 0) {
         this.isFading = false;
       }
@@ -106,15 +108,22 @@ class ShootingStar extends Star {
     this.pos = createVector(random(- height / 2, width * 0.8), 0);
     this.vel = p5.Vector.fromAngle(radians(random(45, 50)), 1);
   }
+
   display() {
     noStroke();
-    fill(255, 255, 255, this.opacity);
+    fill(0, 0, 200, 150);
     let dir;
     for (let i = 1; i < 10; i += 1) {
-      dir = this.vel.setMag(i * 3);
+      dir = this.vel.setMag( i * 3);
       ellipse(this.pos.x + dir.x, this.pos.y + dir.y, this.r * i / 3);
+      fill(0 + i * 25,0 + i * 25,255, 150);
     }
+
+    stroke(0, 0, 200, 150);
+    strokeWeight(this.r);
+    line(this.pos.x+5, this.pos.y+5, this.pos.x - this.r, this.pos.y - this.r);
   }
+
   move() {
     this.pos.add(this.vel.setMag(this.r * 5));
     if ((this.pos.x < - 30) || (this.pos.x > width + 30) || (this.pos.y > height + 30) || (this.pos.y < - 30)) {
@@ -123,13 +132,6 @@ class ShootingStar extends Star {
     }
   }
 }
-
-// function mouseClicked() {
-//   let pos = createVector(mouseX, mouseY);
-//   let vel = p5.Vector.fromAngle(radians(50), 2);
-//   let shootingStar = new ShootingStar(pos, vel);
-//   shootingStars.push(shootingStar);
-// }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
